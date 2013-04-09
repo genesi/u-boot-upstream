7d3cd97ed51af04188ce0c4feb738b406d6a0b8b
Matt Sealey <matt@genesi-usa.com>
2013-04-09 15:52:53 -0500

    efikamx: add Efika MX Slimbook board support
    The Efika MX Slimbook (a.k.a. the "Efika MX53") is a small (100mm x 50mm),
    high-density, high-integration design around the Freescale i.MX53 SoC,
    with a focus on cost-reduction. The board targets Netbook and Industrial
    use cases with integrated LVDS panels and batteries.
    
    This adds basic platform support including MMC, USB support and basic
    booting as per Genesi's production boot model for U-Boot, as streamed
    from the Housekeeping MCU on-board.
    
    Signed-off-by: Matt Sealey <matt@genesi-usa.com>

---
+++ board/genesi/mx53_efikasb/mx53_efikasb.c
@@ -0,0 +1,231 @@
+/*
+ * Copyright (C) 2011 Freescale Semiconductor, Inc.
+ * Jason Liu <r64343@freescale.com>
+ *
+ * Copyright (C) 2011-2013 Genesi USA, Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <common.h>
+#include <asm/io.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/arch/crm_regs.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/iomux-mx53.h>
+#include <asm/arch/clock.h>
+#include <asm/errno.h>
+#include <mmc.h>
+#include <fsl_esdhc.h>
+#include <asm/gpio.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int dram_init(void)
+{
+	gd->ram_size = get_ram_size((void *)CONFIG_SYS_SDRAM_BASE, PHYS_SDRAM_1_SIZE);
+
+	return 0;
+}
+
+/*
+ * for CONFIG_SYS_REVISION_TAG
+ */
+u32 get_board_rev(void)
+{
+	return 0x53000;
+}
+
+/*
+ * USB Configuration
+ */
+static iomux_v3_cfg_t const efikasb_usb_pads[] = {
+	MX53_PAD_KEY_COL4__GPIO4_14,    /* USBH1 OC */
+	MX53_PAD_KEY_ROW4__GPIO4_15,    /* USBH1 EN */
+	MX53_PAD_EIM_D30__GPIO3_30,     /* USBH2 OC */
+	MX53_PAD_EIM_D31__GPIO3_31,     /* USBH2 EN */
+};
+
+#define EFIKASB_USBH1_OC	IMX_GPIO_NR(4, 14)
+#define EFIKASB_USBH1_EN	IMX_GPIO_NR(4, 15)
+#define EFIKASB_USBH2_OC	IMX_GPIO_NR(3, 30)
+#define EFIKASB_USBH2_EN	IMX_GPIO_NR(3, 31)
+
+int board_ehci_hcd_init(int port)
+{
+	return 0;
+}
+
+
+
+/*
+ * SDHC configuration
+ *
+ * SD1 is connected to the on-board SDHC slot
+ * SD2 is connected to the on-board Wifi
+ */
+static iomux_v3_cfg_t const efikasb_sdhc_pads[] = {
+	MX53_PAD_SD1_CMD__ESDHC1_CMD,
+	MX53_PAD_SD1_CLK__ESDHC1_CLK,
+	MX53_PAD_SD1_DATA0__ESDHC1_DAT0,
+	MX53_PAD_SD1_DATA1__ESDHC1_DAT1,
+	MX53_PAD_SD1_DATA2__ESDHC1_DAT2,
+	MX53_PAD_SD1_DATA3__ESDHC1_DAT3,
+	MX53_PAD_GPIO_9__GPIO1_9,
+
+	MX53_PAD_SD2_CMD__ESDHC2_CMD,
+	MX53_PAD_SD2_CLK__ESDHC2_CLK,
+	MX53_PAD_SD2_DATA0__ESDHC2_DAT0,
+	MX53_PAD_SD2_DATA1__ESDHC2_DAT1,
+	MX53_PAD_SD2_DATA2__ESDHC2_DAT2,
+	MX53_PAD_SD2_DATA3__ESDHC2_DAT3,
+};
+
+#define EFIKASB_SDHC1_WP	IMX_GPIO_NR(1, 9)
+
+struct fsl_esdhc_cfg esdhc_cfg[2] = {
+	{MMC_SDHC1_BASE_ADDR},
+	{MMC_SDHC2_BASE_ADDR},
+};
+
+int board_mmc_getcd(struct mmc *mmc)
+{
+	/* Card Detect is not implemented on the board */
+	return 1;
+}
+
+int board_mmc_init(bd_t *bis)
+{
+	u32 index;
+	s32 status = 0;
+
+	esdhc_cfg[0].sdhc_clk = mxc_get_clock(MXC_ESDHC_CLK);
+	esdhc_cfg[1].sdhc_clk = mxc_get_clock(MXC_ESDHC2_CLK);
+
+	for (index = 0; index < CONFIG_SYS_FSL_ESDHC_NUM; index++) {
+		switch (index) {
+		case 0:
+			/* SDHC slot */
+			break;
+		case 1:
+			/* Wifi adapter (SDIO, probably not useful here) */
+			break;
+		default:
+			return status;
+		}
+		status |= fsl_esdhc_initialize(bis, &esdhc_cfg[index]);
+	}
+
+	return status;
+}
+
+
+
+/*
+ * UART configuration
+ *
+ * UART1 is on test pads connected to the i.MX
+ * UART2 is connected to the Housekeeping MCU
+ */
+static iomux_v3_cfg_t const efikasb_uart_pads[] = {
+	MX53_PAD_PATA_DIOW__UART1_TXD_MUX,
+	MX53_PAD_PATA_DMACK__UART1_RXD_MUX,
+
+	MX53_PAD_PATA_DMARQ__UART2_TXD_MUX,
+	MX53_PAD_PATA_BUFFER_EN__UART2_RXD_MUX,
+};
+
+static iomux_v3_cfg_t const efikasb_nand_pads[] = {
+	MX53_PAD_NANDF_CLE__EMI_NANDF_CLE,
+	MX53_PAD_NANDF_ALE__EMI_NANDF_ALE,
+	MX53_PAD_NANDF_WP_B__EMI_NANDF_WP_B,
+	MX53_PAD_NANDF_WE_B__EMI_NANDF_WE_B,
+	MX53_PAD_NANDF_RE_B__EMI_NANDF_RE_B,
+	MX53_PAD_NANDF_RB0__EMI_NANDF_RB_0,
+	MX53_PAD_NANDF_CS0__EMI_NANDF_CS_0,
+	MX53_PAD_NANDF_CS1__EMI_NANDF_CS_1,
+	MX53_PAD_EIM_DA0__EMI_NAND_WEIM_DA_0,
+	MX53_PAD_EIM_DA1__EMI_NAND_WEIM_DA_1,
+	MX53_PAD_EIM_DA2__EMI_NAND_WEIM_DA_2,
+	MX53_PAD_EIM_DA3__EMI_NAND_WEIM_DA_3,
+	MX53_PAD_EIM_DA4__EMI_NAND_WEIM_DA_4,
+	MX53_PAD_EIM_DA5__EMI_NAND_WEIM_DA_5,
+	MX53_PAD_EIM_DA6__EMI_NAND_WEIM_DA_6,
+	MX53_PAD_EIM_DA7__EMI_NAND_WEIM_DA_7,
+};
+
+int board_early_init_f(void)
+{
+	/*
+	 * We know that we have good power or the Housekeeping MCU
+	 * would not have booted the i.MX - in case fuses have been
+	 * blown to reduce CPU and DDR speed at boot time, increase
+	 * them here.. this is not really liable to fail.
+	 */
+
+	mxc_set_clock(MXC_HCLK, 1000, MXC_ARM_CLK);
+	mxc_set_clock(MXC_HCLK, 400, MXC_PERIPH_CLK);
+	mxc_set_clock(MXC_HCLK, 400, MXC_DDR_CLK);
+
+	/*
+	 * Now, get everything we can set up - as early as possible
+	 */
+
+	imx_iomux_v3_setup_multiple_pads(efikasb_uart_pads,
+			ARRAY_SIZE(efikasb_uart_pads));
+
+	imx_iomux_v3_setup_multiple_pads(efikasb_sdhc_pads,
+			ARRAY_SIZE(efikasb_sdhc_pads));
+
+	gpio_direction_input(EFIKASB_SDHC1_WP);
+
+	imx_iomux_v3_setup_multiple_pads(efikasb_usb_pads,
+			ARRAY_SIZE(efikasb_usb_pads));
+
+	gpio_direction_input(EFIKASB_USBH1_OC);
+	gpio_direction_output(EFIKASB_USBH1_EN, 1);
+
+	gpio_direction_input(EFIKASB_USBH2_OC);
+	gpio_direction_output(EFIKASB_USBH2_EN, 1);
+
+	/*
+	 * What's missing? CCM_CLKO, resets, AUDMUX, CSI, LVDS
+	 */
+
+	return 0;
+}
+
+int board_init(void)
+{
+	gd->bd->bi_boot_params = CONFIG_SYS_SDRAM_BASE + 0x100;
+
+	return 0;
+}
+
+int board_late_init(void)
+{
+	return 0;
+}
+
+int checkboard(void)
+{
+	puts("Board: Genesi Efika MX Slimbook\n");
+
+	return 0;
+}
---
+++ include/configs/mx53_efikasb.h
@@ -0,0 +1,306 @@
+/*
+ * Copyright (C) 2011 Freescale Semiconductor, Inc.
+ * Jason Liu <r64343@freescale.com>
+ *
+ * Copyright (C) 2011-2013 Genesi USA, Inc.
+ *
+ * Configuration settings for Genesi Efika MX Slimbook.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include <config_cmd_default.h>
+#include <timestamp.h>
+
+/*
+ * Booting on the Efika MX Slimbook board is kind of a strange monster compared
+ * to development boards and the original Efika MX systems - boot source is
+ * configured by an external MCU and in most instance streams a stage0
+ * bootloader over UART2 to the i.MX53. Then the next stage is booted from NAND
+ * or SD card.
+ *
+ * Alternatively it can be forced to boot from eFUSE settings which will
+ * override this process completely and require the standard i.MX boot method
+ * (DCD, ROM plugins and all..)
+ *
+ * All we're taking care of here is the stream-over-UART method. The U-Boot
+ * binary needs to stick within a hardcoded 256KiB limit (this code with this
+ * value is stored inside the MCU, not easily changed), so we need to keep the
+ * size down by removing some features..
+ */
+
+#undef CONFIG_ZLIB
+#undef CONFIG_GZIP
+#undef CONFIG_BOOTM_NETBSD
+#undef CONFIG_BOOTM_RTEMS
+#undef CONFIG_CMD_SETGETDCR
+#undef CONFIG_CMD_PCMCIA
+#undef CONFIG_PCMCIA
+#undef CONFIG_CMD_LOADB
+#undef CONFIG_CMD_LOADS
+#undef CONFIG_CMD_LOADY
+#undef CONFIG_CMD_IMLS
+
+/*
+ * Using Thumb2 saves us a lot more space.. but doesn't chain properly here
+ */
+//#define CONFIG_SYS_THUMB_BUILD
+
+/*
+ * We will need to undefine this to save even more space if we break the 256KiB
+ * limit, but for now we just squeak in and it would be nicer to users if space
+ * was optimized elsewhere first.
+ */
+#define CONFIG_SYS_LONGHELP
+
+/*
+ * We want to explicitly enable these in case they're not.
+ */
+#define CONFIG_CMD_CACHE
+#define CONFIG_OF_LIBFDT
+#define CONFIG_CMD_BOOTZ
+
+#define CONFIG_MX53
+#define CONFIG_DISPLAY_CPUINFO
+#define CONFIG_DISPLAY_BOARDINFO
+
+#define CONFIG_MACH_TYPE		MACH_TYPE_MX53_EFIKASB
+#define CONFIG_EFIKAMX_MODEL	"slimbook"
+
+#include <asm/arch/imx-regs.h>
+
+#define CONFIG_CMDLINE_TAG
+#define CONFIG_INITRD_TAG
+#define CONFIG_SETUP_MEMORY_TAGS
+#define CONFIG_REVISION_TAG
+
+#define CONFIG_MXC_GPIO
+
+#define CONFIG_MXC_UART
+#define CONFIG_MXC_UART_BASE	UART1_BASE
+#define CONFIG_CONS_INDEX		1
+#define CONFIG_BAUDRATE			115200
+
+/*
+ * Genesi do not support saving the U-Boot environment at runtime and in
+ * any case, there is nowhere to store it..
+ */
+#define CONFIG_SYS_NO_FLASH
+#define CONFIG_ENV_IS_NOWHERE
+
+#define CONFIG_CMD_MMC
+#ifdef CONFIG_CMD_MMC
+#	define CONFIG_MMC
+#	define CONFIG_GENERIC_MMC
+#	define CONFIG_FSL_ESDHC
+#	define CONFIG_SYS_FSL_ESDHC_ADDR	0
+#	define CONFIG_SYS_FSL_ESDHC_NUM		1
+#endif /* CONFIG_CMD_MMC */
+
+#define CONFIG_CMD_USB
+#ifdef CONFIG_CMD_USB
+#	define CONFIG_USB_EHCI
+#	define CONFIG_USB_EHCI_MX5
+#	define CONFIG_USB_STORAGE
+#	undef CONFIG_USB_KEYBOARD
+
+#	define CONFIG_MXC_USB_PORT	1
+#	if (CONFIG_MXC_USB_PORT == 1)
+#		define CONFIG_MXC_USB_PORTSC	(2 << 30)
+#		define CONFIG_MXC_USB_FLAGS		0
+#	endif
+/*
+ * Networking depends on USB for the Efika MX Slimbook, since there is only
+ * an SDIO WiFi card on-board, so we put the network configuration in here
+ * for readability's sake.
+ */
+#	define CONFIG_CMD_NET
+#	ifdef CONFIG_CMD_NET
+#		define CONFIG_USB_HOST_ETHER
+#		define CONFIG_USB_ETHER_ASIX /* Efika MX Smarttop built-in */
+#		define CONFIG_USB_ETHER_SMSC95XX
+#		define CONFIG_CMD_PING
+#		define CONFIG_CMD_DHCP
+#		define CONFIG_CMD_NFS
+#	endif /* CONFIG_CMD_NET */
+#endif /* CONFIG_CMD_USB */
+
+#if defined(CONFIG_CMD_USB) || defined(CONFIG_CMD_MMC)
+#	define CONFIG_DOS_PARTITION
+#	define CONFIG_EFI_PARTITION
+#	define CONFIG_CMD_FAT
+#	define CONFIG_CMD_EXT2
+#	define CONFIG_CMD_EXT4
+#endif /* CONFIG_CMD_USB || CONFIG_CMD_MMC */
+
+/*
+ * Physical Memory Map
+ */
+#define CONFIG_NR_DRAM_BANKS	1
+#define PHYS_SDRAM_1			CSD0_BASE_ADDR
+#define PHYS_SDRAM_1_SIZE		(512 * 1024 * 1024)
+
+#define CONFIG_SYS_SDRAM_BASE		(PHYS_SDRAM_1)
+#define CONFIG_SYS_INIT_RAM_ADDR	(IRAM_BASE_ADDR)
+#define CONFIG_SYS_INIT_RAM_SIZE	(IRAM_SIZE)
+
+#define CONFIG_SYS_INIT_SP_OFFSET	(CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE)
+#define CONFIG_SYS_INIT_SP_ADDR		(CONFIG_SYS_INIT_RAM_ADDR + CONFIG_SYS_INIT_SP_OFFSET)
+
+/*
+ * ENVIRONMENT
+ *
+ * layout of load addresses;
+ *  0x70000000 = START OF MEMORY
+ *  0x70007FC0 = load address for uImage (8MiB+64)
+ *  0x707FFFC0 = load address for uInitrd (16MiB+64)
+ *  0x71800000 = load address for optional device tree (64KiB)
+ *  0x71810000 = other things (scripts, etc.)
+ *
+ * A note about curious address layout; we drop the kernel in 64 bytes before it's
+ * entry point address hardcoded into uImage and kernel, so that the entry point is
+ * actually at the right location.
+ *
+ * We drop the ramdisk 64 bytes back too for the same reason. although there is no
+ * good reason to do this, it does mean the ramdisk data always starts 8MiB into
+ * the memory map.
+ *
+ * The device tree load address is so placed that there is a reserved area for a
+ * MAXIMUM of a 16MiB compressed ramdisk (without overwriting the DT). If your boot
+ * process needs anything different to this, then the script load address is the
+ * only one you need to care about; just use the boot.scr to modify the load addresses
+ * and do what you like, this values are only used to effect more efficient scripting.
+ */
+#define CONFIG_KERNEL_ADDR	0x70007FC0
+#define CONFIG_RD_ADDR		0x707FFFC0
+#define CONFIG_DT_ADDR		0x71800000
+#define CONFIG_SCRIPT_ADDR	0x71810000
+#define CONFIG_LOADADDR		CONFIG_SCRIPT_ADDR
+
+#define CONFIG_STACKSIZE	(128 * 1024)
+#define CONFIG_ENV_SIZE		(8 * 1024)
+#define CONFIG_SYS_MALLOC_LEN	(CONFIG_ENV_SIZE + 2 * 1024 * 1024)
+
+/*
+ * Ideally we would want the load address define to be different to the kernel
+ * address. This will allow errant scripting defaults to run (i.e. fatload
+ * filename) without trashing specially-defined memory areas as above. However,
+ * we have a legacy of users and Linux images to support, so we just suck it up
+ * and deal with it.
+ */
+#define CONFIG_SYS_TEXT_BASE	0x73800000
+#define CONFIG_SYS_LOAD_ADDR	CONFIG_KERNEL_ADDR
+/*
+ * Mmeory test range: from where ATAGs get put, to 64MiB above the ramdisk load
+ * address. If any of this range fails it's a good indicator of serious
+ * breakage.
+ */
+#define CONFIG_SYS_MEMTEST_START	0x70000100
+#define CONFIG_SYS_MEMTEST_END		CONFIG_SYS_TEXT_BASE
+
+#define CONFIG_BOOTDELAY		1
+
+#define CONFIG_BOARD_EARLY_INIT_F
+#define CONFIG_BOARD_LATE_INIT
+
+#define CONFIG_SYS_HUSH_PARSER
+#define CONFIG_SYS_PROMPT		"Slimbook> "
+#define CONFIG_AUTO_COMPLETE
+#define CONFIG_CMDLINE_EDITING
+#define CONFIG_SYS_CBSIZE		1024
+#define CONFIG_SYS_PBSIZE		(CONFIG_SYS_CBSIZE + sizeof(CONFIG_SYS_PROMPT) + 16)
+#define CONFIG_SYS_MAXARGS		16
+#define CONFIG_SYS_BARGSIZE		CONFIG_SYS_CBSIZE
+
+#define CONFIG_SYS_HZ			1000
+
+/*
+ * Do NOT change the names of kerneladdr, scriptaddr, ramdiskaddr or existing
+ * EfikaMX Linux support (i.e. old boot.scr) will quit working.
+ */
+
+#define XADDR(a) #a
+#define ADDR(a) XADDR(a)
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"fdt_high=0xffffffff\0" \
+	"initrd_high=0xffffffff\0" \
+	"model=" CONFIG_EFIKAMX_MODEL "\0" \
+	"firmware_version=" U_BOOT_TIMESTAMP "\0" \
+	"bootscript=boot.scr\0" \
+	"kerneladdr=" ADDR(CONFIG_KERNEL_ADDR) "\0" \
+	"ramdiskaddr=" ADDR(CONFIG_RD_ADDR) "\0" \
+	"dtbaddr=" ADDR(CONFIG_DT_ADDR) "\0" \
+	"scriptaddr=" ADDR(CONFIG_SCRIPT_ADDR) "\0" \
+	"bootdevices=mmc\0" \
+	"units=0\0" \
+	"console=ttymxc0,115200n8\0" \
+	"bootargs=console=${console}"
+
+/*
+ * Note: make sure the last EXTRA_ENV_SETTINGS item has no null terminator!
+ */
+
+/*
+ * Derived from the production-shipped U-Boot from Genesi, and updated to
+ * support new features. This is somewhat of a standard-in-progress for the
+ * Linaro Cross-Distro people and attempts to prove a working configuration
+ * while staying completely compatible with the old config.
+ *
+ * Note: recommended SD card layout is 3 partitions, configured such that the
+ * first partition starts at the 1024 byte position on SD. This way an SD card
+ * can be created to boot U-Boot from SD (with correct DIP settings) by simply
+ * dd'ing to the first partition without creating strange environments with
+ * empty space.
+ *
+ * Second partition is suggested as /boot - and should be ext2 or ext3 so older
+ * U-Boot can read it.
+ *
+ * Third partition is / in this case, and could be ext4.
+ *
+ * However in the event you make a custom SD card with one partition formatted
+ * as ext4, or plug in a FAT-formatted USB stick on an otherwise empty system,
+ * it will all still work. We limit it to a single USB stick, but the default
+ * configuration above does not add "usb" to the devices right now.
+ */
+#define CONFIG_BOOTCOMMAND \
+	"for device in ${bootdevices}; do " \
+		"if test ${device} = \"ide\"; then " \
+			"ide reset; setenv units \"0\"; " \
+		"elif test ${device} = \"mmc\"; then " \
+			"mmc rescan; setenv units \"1 0\"; " \
+		"elif test ${device} = \"usb\"; then " \
+			"usb start; setenv units \"0\"; " \
+		"fi; " \
+		"for unit in ${units}; do " \
+			"${device} dev ${unit}; " \
+			"for part in \"1 2 3\"; do " \
+				"for fs in \"ext4 ext2 fat\"; do " \
+					"setenv loadcmd \"${fs}load ${device} ${unit}:${part}\"; " \
+					"if ${loadcmd} ${scriptaddr} ${bootscript}; then " \
+						"if imi ${scriptaddr}; then " \
+							"source ${scriptaddr}; " \
+						"fi; " \
+					"fi; " \
+				"done; " \
+			"done; " \
+		"done; " \
+	"done; "
+
+#endif /* __CONFIG_H */
